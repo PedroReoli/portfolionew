5b80bc5333ebda6079aa2362134739a47a4f09a1
PedroReoli <pedrolucas2012@gmail.com>
2024-08-12 22:57:13 -0300

    Update Topbar.tsx

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,10 +1,16 @@
 // src/components/Topbar.tsx
 
+
 const Topbar = () => {
   return (
-    <header className="bg-gradient-to-b from-black to-[#111111] p-4 sticky top-0 z-50 shadow-md border-b-2 border-gray-700 ">
+    <header className="bg-gradient-to-b from-black to-[#111111] p-4 sticky top-0 z-50 shadow-md border-b-2 border-gray-700">
       <div className="container mx-auto flex justify-between items-center">
-        <img src="/assets/logo.svg" alt="logo" className="w-36 h-auto" />
+        {/* Logo */}
+        <div className="text-2xl font-bold">
+          &lt; PedroReoli /&gt;
+        </div>
+
+        {/* Menu */}
         <nav className="hidden md:flex space-x-6">
           <a href="#blog" className="text-white hover:text-blue-500 transition">Blog</a>
           <a href="https://www.youtube.com/@DevDesenvolvimento" className="text-white hover:text-blue-500 transition">Youtube</a>
