eaddfc8d80d2808844d68aee8c57df5761a5dc10
PedroReoli <pedrolucas2012@gmail.com>
2024-08-21 04:55:48 -0300

    Skills

--- src/_root/pages/Skills.tsx
+++ src/_root/pages/Skills.tsx
@@ -1,6 +1,4 @@
-// src/_root/pages/Skills.tsx
-
-import  { useState } from 'react';
+import { useState } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { motion, AnimatePresence } from 'framer-motion';
 import { skillsData, Skill } from '@/constants';
@@ -8,6 +6,7 @@ import { skillsData, Skill } from '@/constants';
 const Skills = () => {
   const [currentSkill, setCurrentSkill] = useState<Skill | null>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [activeSkill, setActiveSkill] = useState<number | null>(null);
   const itemsPerPage = 6;
 
   const getLevelDescription = (level: number): string => {
@@ -30,6 +29,11 @@ const Skills = () => {
     setCurrentIndex((prevIndex) => Math.min(prevIndex + itemsPerPage, skillsData.length - itemsPerPage));
   };
 
+  const handleSkillClick = (index: number) => {
+    setActiveSkill(activeSkill === index ? null : index);
+    setCurrentSkill(activeSkill === index ? null : skillsData[index]);
+  };
+
   return (
     <section id="skills" className="bg-[#111111] text-white py-20 px-5">
       <div className="container mx-auto flex flex-col lg:flex-row space-y-8 lg:space-y-0 lg:space-x-8">
@@ -39,59 +43,89 @@ const Skills = () => {
             {currentSkill ? (
               <motion.div
                 key={currentSkill.title}
-                initial={{ y: 10, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: -10, opacity: 0 }}
-                transition={{ duration: 0.2 }}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.3 }}
                 className="space-y-4"
               >
                 <motion.p className="text-lg text-gray-300 font-semibold">{currentSkill.description}</motion.p>
                 <motion.p className="text-lg text-gray-300">
-                  <span className="text-blue-500">Projeto:</span> {currentSkill.project}
+                  <span className="text-blue-500">Detalhes:</span> {currentSkill.details}
                 </motion.p>
-                <motion.p className="text-lg text-gray-300">
-                  <span className="text-blue-500">Vantagens:</span> {currentSkill.advantages}
-                </motion.p>
-                <motion.p className="text-lg text-gray-300">
-                  <span className="text-blue-500">Onde usar:</span> {currentSkill.usage}
-                </motion.p>
+
+                <div className="course-cert-container">
+                  <p className="text-blue-500 mb-2">Cursos:</p>
+                  <div className="course-cert-grid">
+                    {currentSkill.courses.map((course, i) => (
+                      <a
+                        key={i}
+                        href={currentSkill.coursesLinks[i]}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="course-cert-item"
+                      >
+                        <span className="course-cert-title">{course}</span>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+
+                <div className="course-cert-container">
+                  <p className="text-blue-500 mb-2">Certificados:</p>
+                  <div className="course-cert-grid">
+                    {currentSkill.certificates.map((certificate, i) => (
+                      <a
+                        key={i}
+                        href={currentSkill.certificatesLinks[i]}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="course-cert-item"
+                      >
+                        <span className="course-cert-title">{certificate}</span>
+                      </a>
+                    ))}
+                  </div>
+                </div>
               </motion.div>
             ) : (
-              <p className="text-lg text-gray-300">Passe o cursor sobre uma habilidade para ver os detalhes.</p>
+              <p className="text-lg text-gray-300">Clique em uma habilidade para ver os detalhes.</p>
             )}
           </AnimatePresence>
         </div>
         <div className="flex-1 relative">
           <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
             {skillsData.slice(currentIndex, currentIndex + itemsPerPage).map((skill, index) => (
-              <article
+              <motion.article
                 key={index}
-                className="bg-gray-900 p-5 rounded-lg flex flex-col items-center group relative cursor-pointer neon-hover transition-transform transform hover:scale-105"
-                onMouseEnter={() => setCurrentSkill(skill)}
+                className={`bg-gray-900 p-5 rounded-lg flex flex-col items-center group relative cursor-pointer neon-hover transition-transform transform ${activeSkill === index ? 'scale-105 skill-card-active' : ''}`}
+                onClick={() => handleSkillClick(index)}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.3 }}
               >
-                <img src={skill.icon} alt={`ícone do ${skill.title}`} className="mb-4 w-24 h-24" />
+                <img src={skill.icon} alt={`ícone do ${skill.title}`} className="card-icon" />
                 <p className="text-lg font-bold">{skill.title}</p>
                 <div className="w-full bg-gray-700 rounded-full h-2.5 mb-4">
                   <div className={`bg-blue-600 h-2.5 rounded-full`} style={{ width: `${(skill.level / 3) * 100}%` }}></div>
                 </div>
                 <p className="text-sm">{getLevelDescription(skill.level)}</p>
-              </article>
+              </motion.article>
             ))}
           </div>
           {currentIndex > 0 && (
             <button 
-              className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-blue-600 p-3 rounded-full shadow-lg hover:bg-blue-400 transition-colors"
+              className="navigation-button navigation-button-left bg-blue-600"
               onClick={handlePrevSkill}
-              style={{ zIndex: 10 }}
             >
               <FaChevronLeft className="text-white" />
             </button>
           )}
           {currentIndex < skillsData.length - itemsPerPage && (
             <button 
-              className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-600 p-3 rounded-full shadow-lg hover:bg-blue-400 transition-colors"
+              className="navigation-button navigation-button-right bg-blue-600"
               onClick={handleNextSkill}
-              style={{ zIndex: 10 }}
             >
               <FaChevronRight className="text-white" />
             </button>
--- src/constants/skillsData.ts
+++ src/constants/skillsData.ts
@@ -1,122 +1,148 @@
-// src/constants/skillsData.ts
-
 export interface Skill {
-    title: string;
-    icon: string;
-    description: string;
-    level: number;
-    project: string;
-    advantages: string;
-    usage: string;
+  title: string;
+  icon: string;
+  description: string;
+  level: number;
+  details: string;
+  courses: string[]; 
+  coursesLinks: string[]; 
+  certificates: string[]; 
+  certificatesLinks: string[]; 
+}
+
+
+export const skillsData: Skill[] = [
+  { 
+    title: 'HTML', 
+    icon: 'assets/html-icon.svg', 
+    description: 'Linguagem de marcação usada para criar páginas web.', 
+    level: 2.5, 
+    details: 'Fácil de aprender e usar. Essencial para a construção da estrutura de páginas web. Usado em conjunto com CSS e JavaScript para construir e estilizar páginas web.', 
+    courses: ['Curso de HTML Básico', 'Curso de HTML Avançado'], 
+    coursesLinks: ['https://example.com/curso-html-basico', 'https://example.com/curso-html-avancado'], 
+    certificates: ['Certificado de Criação de Sites', 'Certificado Avançado de HTML'], 
+    certificatesLinks: ['https://example.com/certificado-html', 'https://example.com/certificado-html-avancado']
+  },
+  { 
+    title: 'CSS', 
+    icon: 'assets/css-icon.svg', 
+    description: 'Estilos para páginas web, responsáveis pela aparência.', 
+    level: 2, 
+    details: 'Permite separar o conteúdo da apresentação visual. Facilita a manutenção e o design responsivo. Usado para estilizar páginas web, definindo layouts, cores, fontes, e outros aspectos visuais.', 
+    courses: ['Curso de CSS Avançado', 'Curso de CSS Flexbox'], 
+    coursesLinks: ['https://example.com/curso-css-avancado', 'https://example.com/curso-css-flexbox'], 
+    certificates: ['Certificado de Design Web', 'Certificado de CSS Flexbox'], 
+    certificatesLinks: ['https://example.com/certificado-css', 'https://example.com/certificado-css-flexbox']
+  },
+  { 
+    title: 'JavaScript', 
+    icon: 'assets/js-icon.svg', 
+    description: 'Linguagem de programação usada para adicionar interatividade.', 
+    level: 2.0, 
+    details: 'Versátil e amplamente suportado. Permite criar experiências de usuário dinâmicas. Usado para adicionar interatividade a páginas web, como validação de formulários, animações, e comunicação assíncrona.', 
+    courses: ['Curso de JavaScript Completo', 'Curso de JavaScript Avançado'], 
+    coursesLinks: ['https://example.com/curso-js-completo', 'https://example.com/curso-js-avancado'], 
+    certificates: ['Certificado de Programação JavaScript', 'Certificado de JavaScript Avançado'], 
+    certificatesLinks: ['https://example.com/certificado-js', 'https://example.com/certificado-js-avancado']
+  },
+  { 
+    title: 'TypeScript', 
+    icon: 'assets/ts-icon.svg', 
+    description: 'Superset do JavaScript que adiciona tipagem estática.', 
+    level: 2.0, 
+    details: 'Ajuda a detectar erros em tempo de desenvolvimento. Melhora a manutenção do código. Usado em projetos JavaScript para melhorar a robustez e a escalabilidade do código.', 
+    courses: ['Curso de TypeScript Avançado', 'Curso de TypeScript Básico'], 
+    coursesLinks: ['https://example.com/curso-ts-avancado', 'https://example.com/curso-ts-basico'], 
+    certificates: ['Certificado de TypeScript Avançado', 'Certificado de TypeScript Básico'], 
+    certificatesLinks: ['https://example.com/certificado-ts-avancado', 'https://example.com/certificado-ts-basico']
+  },
+  { 
+    title: 'React', 
+    icon: 'assets/react-icon.svg', 
+    description: 'Biblioteca JavaScript para construir interfaces de usuário.', 
+    level: 2.7, 
+    details: 'Componentização facilita a reutilização de código. Grande comunidade e suporte. Usado para construir interfaces de usuário em aplicações web de página única (SPA).', 
+    courses: ['Curso de Desenvolvimento com React', 'Curso de React Hooks'], 
+    coursesLinks: ['https://example.com/curso-react', 'https://example.com/curso-react-hooks'], 
+    certificates: ['Certificado de Desenvolvimento com React', 'Certificado de React Hooks'], 
+    certificatesLinks: ['https://example.com/certificado-react', 'https://example.com/certificado-react-hooks']
+  },
+  { 
+    title: 'Tailwind CSS', 
+    icon: 'assets/tailwind-icon.svg', 
+    description: 'Framework CSS utilitário para criação de layouts.', 
+    level: 1.5, 
+    details: 'Estilização rápida com classes utilitárias. Alta customização sem sair do HTML. Usado para criar layouts rapidamente com uma abordagem de classes utilitárias.', 
+    courses: ['Curso de Layout com Tailwind CSS', 'Curso de Tailwind Avançado'], 
+    coursesLinks: ['https://example.com/curso-tailwind', 'https://example.com/curso-tailwind-avancado'], 
+    certificates: ['Certificado de Layout com Tailwind CSS', 'Certificado de Tailwind Avançado'], 
+    certificatesLinks: ['https://example.com/certificado-tailwind', 'https://example.com/certificado-tailwind-avancado']
+  },
+  { 
+    title: 'AppwriteDB', 
+    icon: 'assets/appwrite-icon.svg', 
+    description: 'AppWrite é um provedor de Backend as a Service.', 
+    level: 1.5, 
+    details: 'Fácil configuração e gerenciamento de backend. Integração com várias plataformas. Usado para gerenciar backend de aplicações web e móveis sem precisar configurar servidores.', 
+    courses: ['Curso de Backend com Appwrite', 'Curso de Appwrite Avançado'], 
+    coursesLinks: ['https://example.com/curso-appwrite', 'https://example.com/curso-appwrite-avancado'], 
+    certificates: ['Certificado de Backend com Appwrite', 'Certificado de Appwrite Avançado'], 
+    certificatesLinks: ['https://example.com/certificado-appwrite', 'https://example.com/certificado-appwrite-avancado']
+  },
+  { 
+    title: 'Python', 
+    icon: 'assets/python-icon.svg', 
+    description: 'Linguagem de programação versátil usada para diversas aplicações.', 
+    level: 0.8, 
+    details: 'Sintaxe clara e concisa. Grande quantidade de bibliotecas e frameworks. Usado em desenvolvimento web, automação, ciência de dados, inteligência artificial e muito mais.', 
+    courses: ['Curso de Programação em Python', 'Curso de Python para Data Science'], 
+    coursesLinks: ['https://example.com/curso-python', 'https://example.com/curso-python-ds'], 
+    certificates: ['Certificado de Programação em Python', 'Certificado de Python para Data Science'], 
+    certificatesLinks: ['https://example.com/certificado-python', 'https://example.com/certificado-python-ds']
+  },
+  { 
+    title: 'C#', 
+    icon: 'assets/csharp-icon.svg', 
+    description: 'Linguagem de programação usada para desenvolvimento de aplicações desktop e web.', 
+    level: 1.5, 
+    details: 'Integrado com o ecossistema Microsoft. Forte tipagem e orientação a objetos. Usado para desenvolvimento de aplicações desktop, web, e jogos com Unity.', 
+    courses: ['Curso de Desenvolvimento em C#', 'Curso de C# para Unity'], 
+    coursesLinks: ['https://example.com/curso-csharp', 'https://example.com/curso-csharp-unity'], 
+    certificates: ['Certificado de Desenvolvimento em C#', 'Certificado de C# para Unity'], 
+    certificatesLinks: ['https://example.com/certificado-csharp', 'https://example.com/certificado-csharp-unity']
+  },
+  { 
+    title: 'Docker', 
+    icon: 'assets/docker-icon.svg', 
+    description: 'Plataforma de virtualização de contêineres.', 
+    level: 0.8, 
+    details: 'Facilita a portabilidade e escalabilidade de aplicações. Reduz inconsistências de ambiente. Usado para criar, gerenciar e executar contêineres, permitindo a portabilidade de aplicações entre diferentes ambientes.', 
+    courses: ['Curso de Conteinerização com Docker', 'Curso de Docker Avançado'], 
+    coursesLinks: ['https://example.com/curso-docker', 'https://example.com/curso-docker-avancado'], 
+    certificates: ['Certificado de Conteinerização com Docker', 'Certificado de Docker Avançado'], 
+    certificatesLinks: ['https://example.com/certificado-docker', 'https://example.com/certificado-docker-avancado']
+  },
+  { 
+    title: 'SQL', 
+    icon: 'assets/sql-icon.svg', 
+    description: 'Linguagem de consulta usada para gerenciar bancos de dados relacionais.', 
+    level: 0.8, 
+    details: 'Permite consultas complexas e manipulação de grandes volumes de dados. Suporte amplo. Usado para gerenciar e manipular bancos de dados relacionais, como MySQL, PostgreSQL e SQL Server.', 
+    courses: ['Curso de Gestão de Bancos de Dados com SQL', 'Curso de SQL Avançado'], 
+    coursesLinks: ['https://example.com/curso-sql', 'https://example.com/curso-sql-avancado'], 
+    certificates: ['Certificado de Gestão de Bancos de Dados com SQL', 'Certificado de SQL Avançado'], 
+    certificatesLinks: ['https://example.com/certificado-sql', 'https://example.com/certificado-sql-avancado']
+  },
+  { 
+    title: 'Git', 
+    icon: 'assets/git-icon.svg', 
+    description: 'Sistema de controle de versão distribuído.', 
+    level: 3, 
+    details: 'Facilita o trabalho colaborativo. Histórico completo de mudanças no código. Usado para versionamento de código, permitindo colaboração em projetos de software.', 
+    courses: ['Curso de Controle de Versão com Git', 'Curso de Git Flow'], 
+    coursesLinks: ['https://example.com/curso-git', 'https://example.com/curso-git-flow'], 
+    certificates: ['Certificado de Controle de Versão com Git', 'Certificado de Git Flow'], 
+    certificatesLinks: ['https://example.com/certificado-git', 'https://example.com/certificado-git-flow']
   }
-  
-  export const skillsData: Skill[] = [
-    { 
-      title: 'HTML', 
-      icon: 'assets/html-icon.svg', 
-      description: 'Linguagem de marcação usada para criar páginas web.', 
-      level: 2.5, 
-      project: 'Criação de um site pessoal.',
-      advantages: 'Fácil de aprender e usar. Essencial para a construção da estrutura de páginas web.',
-      usage: 'Usado em conjunto com CSS e JavaScript para construir e estilizar páginas web.' 
-    },
-    { 
-      title: 'CSS', 
-      icon: 'assets/css-icon.svg', 
-      description: 'Estilos para páginas web, responsáveis pela aparência.', 
-      level: 2, 
-      project: 'Estilização de um blog pessoal.',
-      advantages: 'Permite separar o conteúdo da apresentação visual. Facilita a manutenção e o design responsivo.',
-      usage: 'Usado para estilizar páginas web, definindo layouts, cores, fontes, e outros aspectos visuais.' 
-    },
-    { 
-      title: 'JavaScript', 
-      icon: 'assets/js-icon.svg', 
-      description: 'Linguagem de programação usada para adicionar interatividade.', 
-      level: 2.0, 
-      project: 'Desenvolvimento de uma aplicação web interativa.',
-      advantages: 'Versátil e amplamente suportado. Permite criar experiências de usuário dinâmicas.',
-      usage: 'Usado para adicionar interatividade a páginas web, como validação de formulários, animações, e comunicação assíncrona.' 
-    },
-    { 
-      title: 'TypeScript', 
-      icon: 'assets/ts-icon.svg', 
-      description: 'Superset do JavaScript que adiciona tipagem estática.', 
-      level: 2.0, 
-      project: 'Implementação de um sistema de tipagem em um projeto JS existente.',
-      advantages: 'Ajuda a detectar erros em tempo de desenvolvimento. Melhora a manutenção do código.',
-      usage: 'Usado em projetos JavaScript para melhorar a robustez e a escalabilidade do código.' 
-    },
-    { 
-      title: 'React', 
-      icon: 'assets/react-icon.svg', 
-      description: 'Biblioteca JavaScript para construir interfaces de usuário.', 
-      level: 2.7, 
-      project: 'Criação de uma aplicação SPA com React.',
-      advantages: 'Componentização facilita a reutilização de código. Grande comunidade e suporte.',
-      usage: 'Usado para construir interfaces de usuário em aplicações web de página única (SPA).' 
-    },
-    { 
-      title: 'Tailwind CSS', 
-      icon: 'assets/tailwind-icon.svg', 
-      description: 'Framework CSS utilitário para criação de layouts.', 
-      level: 1.5, 
-      project: 'Estilização de uma aplicação web com Tailwind.',
-      advantages: 'Estilização rápida com classes utilitárias. Alta customização sem sair do HTML.',
-      usage: 'Usado para criar layouts rapidamente com uma abordagem de classes utilitárias.' 
-    },
-    { 
-      title: 'AppwriteDB', 
-      icon: 'assets/appwrite-icon.svg', 
-      description: 'AppWrite é um provedor de Backend as a Service.', 
-      level: 1.5, 
-      project: 'Implementação de backend com Appwrite.',
-      advantages: 'Fácil configuração e gerenciamento de backend. Integração com várias plataformas.',
-      usage: 'Usado para gerenciar backend de aplicações web e móveis sem precisar configurar servidores.' 
-    },
-    { 
-      title: 'Python', 
-      icon: 'assets/python-icon.svg', 
-      description: 'Linguagem de programação versátil usada para diversas aplicações.', 
-      level: 0.8, 
-      project: 'Desenvolvimento de scripts para automação de tarefas.',
-      advantages: 'Sintaxe clara e concisa. Grande quantidade de bibliotecas e frameworks.',
-      usage: 'Usado em desenvolvimento web, automação, ciência de dados, inteligência artificial e muito mais.' 
-    },
-    { 
-      title: 'C#', 
-      icon: 'assets/csharp-icon.svg', 
-      description: 'Linguagem de programação usada para desenvolvimento de aplicações desktop e web.', 
-      level: 1.5, 
-      project: 'Criação de uma aplicação Windows Forms.',
-      advantages: 'Integrado com o ecossistema Microsoft. Forte tipagem e orientação a objetos.',
-      usage: 'Usado para desenvolvimento de aplicações desktop, web, e jogos com Unity.' 
-    },
-    { 
-      title: 'Docker', 
-      icon: 'assets/docker-icon.svg', 
-      description: 'Plataforma de virtualização de contêineres.', 
-      level: 0.8, 
-      project: 'Conteinerização de uma aplicação web.',
-      advantages: 'Facilita a portabilidade e escalabilidade de aplicações. Reduz inconsistências de ambiente.',
-      usage: 'Usado para criar, gerenciar e executar contêineres, permitindo a portabilidade de aplicações entre diferentes ambientes.' 
-    },
-    { 
-      title: 'SQL', 
-      icon: 'assets/sql-icon.svg', 
-      description: 'Linguagem de consulta usada para gerenciar bancos de dados relacionais.', 
-      level: 0.8, 
-      project: 'Design de um banco de dados relacional.',
-      advantages: 'Permite consultas complexas e manipulação de grandes volumes de dados. Suporte amplo.',
-      usage: 'Usado para gerenciar e manipular bancos de dados relacionais, como MySQL, PostgreSQL e SQL Server.' 
-    },
-    { 
-      title: 'Git', 
-      icon: 'assets/git-icon.svg', 
-      description: 'Sistema de controle de versão distribuído.', 
-      level: 3, 
-      project: 'Gerenciamento de versões de um projeto colaborativo.',
-      advantages: 'Facilita o trabalho colaborativo. Histórico completo de mudanças no código.',
-      usage: 'Usado para versionamento de código, permitindo colaboração em projetos de software.' 
-    }
-  ];
+];
+
