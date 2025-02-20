d9f326f7909081eb591ca0d4e99042728010109f
Pedro Lucas  Reis de Oliveira Sousa <128660097+PedroReoli@users.noreply.github.com>
2025-02-20 12:50:39 -0300

    Update index.ts

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -1,7 +1,6 @@
 // src/_root/pages/index.ts
 export { default as Home } from './Home';
 export { default as About } from './About';
-export { default as Experience } from './Experience';
 export { default as Projects } from './Projects';
 export { default as Services } from './Services';
 export { default as Skills } from './Skills';
